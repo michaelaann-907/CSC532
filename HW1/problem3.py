340be08051aec591abc6a11d69911da78fd62227
Michaela Pierce <michaelaap907@gmail.com>
2024-01-25 13:48:29 -0500

    Updated

--- HW1/problem3.py
+++ HW1/problem3.py
@@ -1,24 +1,60 @@
 
 # Insertion-Sort Algorithm
-def insertionSort(a):
+def insertion_sort(a):
     # Iterate through each element in the array starting from the second element
-    for i in range (1, len(a)):
-        key = a[i]  # current element compared & inserted
-        j = i - 1   # index of previous element
+    for j in range (1, len(a)):
+        # current element to be inserted
+        key = a[j]
 
-        # increase elements position by one if greater than key
-        while j >= 0 and key < a[j]:
-            a[j + 1] = a[j]
-            j = j -1
+        # move elements greater than key up one position
+        i = j - 1
+        # loop checks if i is greater than or equal to 0 and key is less than a[i]
+        while i >= 0 and key < a[i]:
+            # shift elements to the right to make space for key
+            a[i + 1] = a[i]
+            i = i -1
 
-        # place key at after the element
-            a[j + 1] = key
+        # place key at correct position in sorted array
+        a[i + 1] = key
+
+# Testing insertion_sort function using different arrays
+a1 = [5, 7, 2, 8, 4, 6]
+a2 = [4, 5 , 10, 11, 3, 2]
+a3 = [10, 12, 15, 5, 0, 8]
+
+# array 1
+print("Unsorted Array 1:", a1)
+# call function
+insertion_sort(a1)
+print("Sorted Array 1:", a1)
+
+# array 2
+print("\nUnsorted Array 2:", a2)
+insertion_sort(a2)
+print("Sorted Array 2:", a2)
+
+# array 3
+print("\nUnsorted Array 3:", a3)
+insertion_sort(a3)
+print("Sorted Array 3:", a3)
 
 
-a= [10, 3, 5, 7, 0]
-print("Unsorted Array: ", a)
-insertionSort(a)
-print("Sorted Array: ", a)
+
+
+
+
+# Merge Algorithm
+
+
+
+
+
+# Merge - Sort Algorithm
+
+
+
+
+
 
 
 
